47267662ead1460d8a8317ebf4aeb40b3725e6f8
Gilvan <gilvanritter@gmail.com>
2019-11-10 04:51:57 -0300

    Display active month in window name / browser tab.

--- src/Components/Wallet.js
+++ src/Components/Wallet.js
@@ -1,10 +1,14 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {GetMonthName} from "../Services/Datetime";
 
 function Wallet(props) {
     const [activeMonth, setMonth] = useState(
         GetMonthName(Date.now())
     );
+
+    useEffect(() => {
+        document.title = activeMonth;
+    });
 
     return (
         <header className="sticky">
